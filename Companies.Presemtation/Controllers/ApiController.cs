dfd90bd9799942afaebe364830379e998ac2b9a2
KarinWegner <karinwegner00@gmail.com>
2024-12-27 08:38:08 +0100

    swagger fix

--- Companies.Presemtation/Controllers/ApiController.cs
+++ Companies.Presemtation/Controllers/ApiController.cs
@@ -6,7 +6,7 @@ namespace Companies.Presemtation.Controllers
 {    
     public class ApiController : ControllerBase
     {
-
+        [NonAction]
         public ActionResult ProcessError(ApiBaseResponse baseResponse)
         {
             return baseResponse switch
