026d5acb32070cf9b2eb05433249cab5191e5f36
KarinWegner <karinwegner00@gmail.com>
2025-01-28 09:23:25 +0100

    First test

---
+++ Companies.Presemtation/ControllersForTest/SimpleController.cs
@@ -0,0 +1,29 @@
+﻿using Companies.Shared.DTOs;
+using Companies.Shared.Request;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Services.Contracts;
+using System.Text.Json;
+
+namespace Companies.Presemtation.Controllers;
+
+[Route("api/simple")]
+[ApiController]
+public class SimpleController : ControllerBase
+{
+   
+
+    public SimpleController()
+    {
+     
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<CompanyDto>>> GetCompany()
+    {     
+
+        return Ok();
+    }
+}
+
